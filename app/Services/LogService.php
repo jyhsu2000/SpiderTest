5c4d210188c79f8e510ef0cf6c91630d5acec1c0
KID <jyhsu2000@yahoo.com.tw>
2016-08-15 16:24:12 +0800

    Revert "Update jsonOptions"
    This reverts commit 42d25ef70a573770805fc1ba540d8888ce45b3be.

--- app/Services/LogService.php
+++ app/Services/LogService.php
@@ -36,8 +36,9 @@ class LogService
         'emergency' => MonologLogger::EMERGENCY,
     ];
 
-    //JSON選項（不跳脫Unicode字元、不跳脫斜線）
-    protected static $jsonOptions = JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES;
+    protected static $jsonOptions =
+        JSON_UNESCAPED_UNICODE              //不跳脫Unicode字元
+        + JSON_UNESCAPED_SLASHES;            //不跳脫斜線
 
     /**
      * Log an message to the logs.
